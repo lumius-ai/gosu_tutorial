8174758570e913491f9b0937613b2a250675c4c6
Razvan Rotundu <raz.rotundu@gmail.com>
2024-10-24 12:20:26 -0400

    Add controllable player

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require_relative "lib/player"
 
 class Tutorial_Game < Gosu::Window
 
@@ -8,16 +9,32 @@ class Tutorial_Game < Gosu::Window
     self.caption = "Tutorial Game"
     
     @background_image = Gosu::Image.new("media/space.png")
+
+    # Create a player in the center position
+    @player = Player.new()
+    @player.warp(320, 240)
   end
 
   # Game logic, collision testing, etc
   def update
-
+    # Button motion controls
+    if Gosu.button_down? Gosu::KB_LEFT or Gosu.button_down? Gosu::GP_LEFT
+      @player.turn_left
+    end
+    if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+      @player.turn_right
+    end
+    if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+      @player.accelerate
+    end
+    @player.move
   end
 
   # Scene draw, but no logic
   def draw
+    @player.draw()
     @background_image.draw(0,0,0)
+
   end
 end
 
